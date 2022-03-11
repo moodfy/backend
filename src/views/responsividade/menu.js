3dffc9ca62731d0fdd957c61142255c1a747211a
laura-bluz <laura.bluz@hotmail.com>
2022-03-11 17:14:03 -0300

    Alterações de Responsividade

---
+++ src/views/responsividade/menu.js
@@ -0,0 +1,33 @@
+
+const btnMobile = document.getElementById('btn-mobile');
+const btnMobileClose = document.getElementById('btn-mobile-close');
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+btnMobileClose.addEventListener('click', toggleMenu);
+btnMobileClose.addEventListener('touchstart', toggleMenu);
+
+
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart')
+        event.preventDefault();
+    const nav = document.getElementById('nav'); 
+    nav.classList.toggle('active');
+  
+}
+
+
+const navlinks = nav.getElementsByTagName('a');
+
+function toggleNav() {
+    (nav.classList.contains('active')) ? nav.classList.remove('active') : nav.classList.add('active');
+  }
+
+for(var i = 0; i < navlinks.length; i++) {
+    navlinks[i].addEventListener('click', function() {
+      toggleNav();
+  });
+}
+
